67289de7d43170c03c47efafb8eceeb5c4f51642
Unknown <mikipmoline@gmail.com>
2018-12-16 23:50:43 +0100

    Funciona pero es poden repetir resultats

--- lsh.cc
+++ lsh.cc
@@ -26,12 +26,37 @@ void out_minhashes (const vector<vector<uint32_t>>& minhashes){
 	}
 }
 
+//per debugar
 void out_vec(const vector<uint32_t> &vec){
 	for (int i = 0; i < vec.size(); ++i)
 		cout << vec[i] << ' ';
 	cout << endl;
 }
 
+//ens retorna true si el document doc es similar a y superant o igualant el threshold
+bool is_candidate(const int& doc, const int& y, const vector<vector<uint32_t>>& candidates, double & sim, const double& threshold){
+	int equal = 0;
+	for (int b = 0; b < candidates.size(); ++b){
+		if (candidates[b][doc] == candidates[b][y]) ++equal;
+	}
+	if ((equal/candidates.size()) >= threshold){
+		sim = (double(equal)/double(candidates.size()));
+		return true;
+	}
+	return false;
+}
+
+//busca un possible candidat en la mateixa banda
+void search_candidate(const int& band, const int& doc, const vector<vector<uint32_t>>& candidates, const double& threshold){
+	for (int y = doc + 1; y < candidates[0].size(); ++y){
+		if (candidates[band][doc] == candidates[band][y]){
+			double sim;
+			if (is_candidate(doc,y,candidates, sim,threshold)){
+				cout << "el document " << doc << " i el docmuent " << y << " son similars en un " << sim << endl;
+			}
+		}
+	}
+}
 
 //funcio de hash per obtenir una posició en el map
 class Hash {
@@ -60,10 +85,13 @@ int main() {
 	out_minhashes(minhashes);
 
 	int band, rows;
+	double threshold;
 	cout << "introdueix el nombre de bandes" << endl;
 	cin >> band;
 	cout << "introdeux el nombre de files per banda" << endl;
 	cin >> rows;
+	cout << "introdueix un threshold" << endl;
+	cin >> threshold;
 
 	vector<vector<uint32_t>> candidates(band,vector<uint32_t>(n));
 
@@ -81,7 +109,7 @@ int main() {
 	}
 
 	//això ens retorna una matriu a on per cada doc té tants elements
-	//com bandes, on cada element és el vector de rows firmes al qual
+	//com bandes, on cada element és el vector de firmes, com rows, al qual
 	//se li ha aplicat una funció de hash per obtenir un identificador únic
 	for (int x = 0; x < n; ++x){
 		cout << "doc" << x << " -> ";
@@ -89,5 +117,18 @@ int main() {
 			cout << candidates[y][x] << ' ';
 		}
 		cout << endl;
+	}
+
+	//agafem cada identificador de cada banda i comparem amb les següents
+	//en busca d'un candidat
+	int aux_i, aux_j;
+	for (int x = 0; x < band; ++x){
+		for (int y = 0; y < n; ++y){
+			if (y != n-1){
+				aux_i = x;
+				aux_j = y;
+				search_candidate(aux_i, aux_j, candidates, threshold);
+			}
+		}
 	}
 }
